c2436bdadb03755d40676b4a9b1930dbb86e4f4b
sameerbhanushali <sameerbhanushali1985@gmail.com>
2023-09-02 01:36:53 +0530

    Sameer Changes
    login.bade and css change

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -14,8 +14,8 @@
   $(window).on('load', function() {
     if (feather) {
       feather.replace({
-        width: 14
-        , height: 14
+        width: 14,
+        height: 14
       })
     }
   })
