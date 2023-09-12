ac392cc93871f65a4c15bfc13d348a70018ad83c
sameerbhanushali <sameerbhanushali1985@gmail.com>
2023-09-13 01:29:37 +0530

    css Changes

--- resources/views/panels/submenu.blade.php
+++ resources/views/panels/submenu.blade.php
@@ -1,13 +1,13 @@
 {{-- For submenu --}}
-<ul class="menu-content">
+<ul class="menu-content dropdown dropdown-menu" aria-labelledby="dropdownMenuButton1">
   @if(isset($menu))
   @foreach($menu as $submenu)
-  <li class="@if(Request::is($submenu->slug.'/*') || Request::is($submenu->slug)) active  @endif" {{ '' }}">
-    <a href="{{isset($submenu->url) ? url($submenu->url):'javascript:void(0)'}}" class="d-flex align-items-center" target="{{isset($submenu->newTab) && $submenu->newTab === true  ? '_blank':'_self'}}">
+  <li class="nav-item @if(Request::is($submenu->slug.'/*') || Request::is($submenu->slug)) active  @endif" {{ '' }}">
+    <a href="{{isset($submenu->url) ? url($submenu->url):'javascript:void(0)'}}" class="nav-link d-flex align-items-center" target="{{isset($submenu->newTab) && $submenu->newTab === true  ? '_blank':'_self'}}">
       @if(isset($submenu->icon))
-      <i data-feather="{{$submenu->icon}}"></i>
+      <i class="menu-icon" data-feather="{{$submenu->icon}}"></i>
       @endif
-      <span class="menu-item text-truncate">{{ $submenu->name }}</span>
+      <span class="menu-title text-truncate">{{ $submenu->name }}</span>
     </a>
     @if (isset($submenu->submenu))
     @include('panels/submenu', ['menu' => $submenu->submenu])
